68bb15a222686b264f79657430a6425a769b3ff6
me <me@test.com>
2024-01-16 18:32:39 +0800

    Finish coding

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
+const container = document.querySelector('.container');
+const hGrids = document.querySelector('input[name="hGrids"]');
+const vGrids = document.querySelector('input[name="vGrids"]');
+const submit = document.querySelector('button[type="button"]');
+
+submit.addEventListener('click', setGrid);
+
 let isMouseDown = false;
+document.addEventListener('mouseup', mouseUp);
 createGrid(50, 50);
 
+function setGrid() {
+    if (Number.isInteger(+hGrids.value) && Number.isInteger(+vGrids.value) &&
+        +hGrids.value <= 100 && +vGrids.value <= 100 &&
+        +hGrids.value > 0 && +vGrids.value > 0) {
+
+        clearGrid();
+        createGrid(hGrids.value, vGrids.value);
+
+    } else {
+        hGrids.value = '';
+        vGrids.value = '';
+        alert('Please input two integer numbers between 1 and 100');
+    }
+}
 
 function createGrid(horizontalNumber, verticalNumber) {
 
     const vDiv = [];
     const hDiv = [];
-    const container = document.querySelector('.container');
 
     for (let i = 0; i < verticalNumber; i++) {
 
@@ -16,7 +37,6 @@ function createGrid(horizontalNumber, verticalNumber) {
             hDiv[j] = document.createElement('div');
             hDiv[j].classList.add('grid');
             hDiv[j].addEventListener('mousedown', mouseDown);
-            hDiv[j].addEventListener('mouseup', mouseUp);
             hDiv[j].addEventListener('mousemove', mouseMove);
             vDiv[i].append(hDiv[j]);
         }
@@ -27,7 +47,14 @@ function createGrid(horizontalNumber, verticalNumber) {
 
 function mouseDown(event) {
     isMouseDown = true;
-    event.target.classList.add('grid-change');
+
+    if (event.target.classList.contains('triggered')) {
+        return;
+    }
+    
+    addRandomColor(event.target);
+
+    event.target.classList.add('triggered');
 }
 
 function mouseUp() {
@@ -35,7 +62,27 @@ function mouseUp() {
 }
 
 function mouseMove(event) {
+    if (event.target.classList.contains('triggered')) {
+        return;
+    }
+
     if (isMouseDown) {
-        event.target.classList.add('grid-change');
+        addRandomColor(event.target);
+        event.target.classList.add('triggered');
+    }
+
+}
+
+function addRandomColor(target) {
+    let color = 'rgb(' +
+        Math.floor(Math.random() * 256) + ',' +
+        Math.floor(Math.random() * 256) + ',' +
+        Math.floor(Math.random() * 256) + ')';
+    target.style.backgroundColor = color;
+}
+
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
     }
 }
